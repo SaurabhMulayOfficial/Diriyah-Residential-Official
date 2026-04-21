a9cec6af3b4c9faadfcd2553234580ca108fa185
Saurabh Mulay <saurabh.mulay@globant.com>
2026-04-21 20:27:42 +0530

    Release/uat/v1.2 21042026 (#179)
    * priortiy edit (#166)
    
    * QA Bug Changes (#167)
    
    * Permission Set Changes (#168)
    
    * Permission Set Changes
    
    * CR Number Permission Changes
    
    * UAT fix (#169)
    
    * Layouts, fields, permission, profile Field Permission (#171)
    
    * Layouts, fields, permission, profile Field Permission
    
    * Readonly true on opportunity
    
    * Field Read access
    
    * Page Layouts, fields, permissions (#172)
    
    * Page Layouts, fields, permissions
    
    * event location field permission
    
    * [ Reassignment Logic ] (#173)
    
    * New Logic Changes
    
    * Lead Routing Changes
    
    * Bind vars
    
    * LWC Changes
    
    * Test Classes + Reassignment logic
    
    * JS edit
    
    * profile updated permissions
    
    ---------
    
    Co-authored-by: Roshan.jambhule <roshan.jambhule@globant.com>
    
    * UAT Bug Changes (#174)
    
    * Profile changes
    
    * Visibility
    
    * removed tab
    
    * Home tab Visibility
    
    * Lead and Opportunity Flexipage changes (#175)
    
    * Layouts and permissions (#176)
    
    * Feature/reassignment logic change (#178)
    
    * Lead reassignment logic
    
    * Test fix
    
    * Fields + Profilr
    
    * Test fix
    
    * FlexiPages, Permissions, Project field Update, Record Type(LEad, opportunity) (#177)
    
    Co-authored-by: Saurabh Mulay <saurabh.mulay@globant.com>
    
    ---------
    
    Co-authored-by: PoonamJain-Official <poonam.jain@globant.com>
    Co-authored-by: NitinMaratheOfficial <nitin.marathe@globant.com>
    Co-authored-by: Roshan.jambhule <roshan.jambhule@globant.com>

---
+++ force-app/main/default/lwc/useravailability/useravailability.js
@@ -0,0 +1,88 @@
+import { LightningElement, track } from 'lwc';
+import updateUserStatus from '@salesforce/apex/RES_UserAvailabilityController.updateUserStatus';
+import getUserStatus from '@salesforce/apex/RES_UserAvailabilityController.getUserStatus';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class UserAvailability extends LightningElement {
+
+    @track currentStatus = '';
+    @track isLoading = false;
+    @track showToast = false;
+    @track toastMessage = '';
+    @track isSuccess = true;
+
+    connectedCallback() {
+        this.loadStatus();
+    }
+
+    loadStatus() {
+        getUserStatus()
+            .then(result => {
+                console.log('result==>',result);
+                this.currentStatus = result;
+            })
+            .catch(error => {
+                this.showError(error);
+            });
+    }
+
+    handleAvailable() {
+        this.updateStatus('Available');
+    }
+
+    handleOffline() {
+        this.updateStatus('Offline');
+    }
+
+    updateStatus(status) {
+        this.isLoading = true;
+
+        updateUserStatus({ newStatus: status })
+            .then(result => {
+                this.currentStatus = result;
+                this.showSuccess('Your Status updated to ' + result);
+            })
+            .catch(error => {
+                this.showError(error);
+            })
+            .finally(() => {
+                this.isLoading = false;
+            });
+    }
+
+    // UI Helpers
+    get isAvailable() {
+        return this.currentStatus === 'Available';
+    }
+
+    get isOffline() {
+        return this.currentStatus === 'Offline';
+    }
+
+    get computedAvailableClass() {
+        return this.isAvailable ? 'btn active available' : 'btn';
+    }
+
+    get computedOfflineClass() {
+        return this.isOffline ? 'btn active offline' : 'btn';
+    }
+
+    showSuccess(msg) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: msg,
+                variant: 'success'
+            })
+        );
+    }
+
+    showError(error) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error',
+                message: error.body?.message || 'Error occurred',
+                variant: 'error'
+            })
+        );
+    }
+}
