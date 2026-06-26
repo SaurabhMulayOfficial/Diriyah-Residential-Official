978e4fcf8467b595072bd8ec74df6a83c07baffd
syed.ashraf <syed.ashraf@globant.com>
2026-06-26 11:08:37 +0530

    Rows per page in converted leads tab

--- force-app/main/default/lwc/resConvertedLeads/resConvertedLeads.js
+++ force-app/main/default/lwc/resConvertedLeads/resConvertedLeads.js
@@ -19,10 +19,10 @@ export default class ResConvertedLeads extends LightningElement {
     _searchTimeout;
 
     pageSizeOptions = [
-        { label: '10', value: '10' },
         { label: '20', value: '20' },
         { label: '50', value: '50' },
-        { label: '100', value: '100' }
+        { label: '100', value: '100' },
+        { label: '200', value: '200' }
     ];
 
     columns = [
